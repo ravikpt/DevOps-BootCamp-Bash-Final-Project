4c10f797cdbc958957bc247e0bdab44338425b3d
Ranjit Kumar <95127402+ravikpt@users.noreply.github.com>
2022-12-27 12:53:03 +0400

    updating Changes - 03 - correct SHABANG

--- transfer.sh
+++ transfer.sh
@@ -1,47 +1,41 @@
-#!/bin/curl
+#!/bin/bash
 
-currentVersion="1.1.0"
+currentVersion="1.23.0"
 
 httpSingleUpload()
 {
-    response=$(curl -A curl -# --upload-file "$1" "https://transfer.sh/$2") || { echo "Failure!"; return 1;}
-    printUploadResponse
+   response=$(curl -A curl -# --upload-file "$1" "https://transfer.sh/$2") || { echo "Failure!"; return 1;}
+   printUploadResponse
 }
 
 printUploadResponse()
-
 {
-
 fileID=$(echo "$response" | cut -d "/" -f 4)
   cat <<EOF
 Transfer File URL: $response
 EOF
-
 }
 
 singleUpload()
 {
+  for i in "$@"
+  do
+    
+    #filePath=$(echo "$i" | sed 's/~/$HOME/g')
+    filePath=$i ; echo "${i//~/$HOME}"
 
- for i in "$@"
- do
-   # filePath=$(echo "$1" | sed s:"~":"$HOME":g)
-   filePath=$i ; echo "${i//~/$HOME}"
+    if [ ! -e "$filePath" ]; then
+    {
+      echo "Error: invalid file path";
+      return 1;
+    };
+    fi
 
-   if [ ! -f "$filePath"]; then
-        {
-                echo "Error: invalid file path";
-                return 1;
-   };
-   fi
+    tempFileName=$(echo "$i" | sed "s/.*\///")
 
-   tempFileName=$(echo "$1" | sed "s/.*\///")
+    echo "Uploading $tempFileName"
 
-   echo "Uploading $tempFileName"
-
-   httpSingleUpload "$filePath" "$tempFileName"
+    httpSingleUpload "$filePath" "$tempFileName"
   done
 }
-
-singleUpload "$1" || exit 1
-
 printUploadResponse
